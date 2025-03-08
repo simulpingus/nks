0ca72596b1693782960d5d32831644f690cab095
Slenderman <ssimulpong@outlook.com>
2025-03-07 20:33:56 -0500

    Update undead.lua

--- undead.lua
+++ undead.lua
@@ -1,3 +1,1 @@
-local guh = game:GetService("Players").LocalPlayer.Character.Humanoid
-guh:SetStateEnabled(Enum.HumanoidStateType.Dead, false)
-guh.Health = -5
+game:GetService("Players").LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Dead, false)
